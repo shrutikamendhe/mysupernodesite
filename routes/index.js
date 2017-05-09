f7ee0b0861303e8301d7fe25ef7c209456484ded
Shrutika Mendhe Patil <shrutika.mendhe@click2cloud.net>
2017-05-09 18:18:41 +0530

    retrieving connection string

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var connectionString = process.env.CUSTOMCONNSTR_DefaultConnection;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World' });
+  res.render('index', { title: connectionString });
 });
 
 module.exports = router;
